1d2c44b99d670bfba1aa7da300204b3100f44e17
Gonzalo Araya <Gonzalo Araya>
2024-07-12 20:44:05 -0300

    manejando estadisticas

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -88,16 +88,30 @@ public class Principal {
                 ));
 
         // Buscar episodios por pedazo del titulo
-        System.out.println("Por favor escriba el titulo del capitulo que desea ver: ");
-        var pedazoTitulo = teclado.nextLine();
-        Optional<Episodio> episodioBuscado = episodios.stream()
-                .filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo.toUpperCase()))
-                .findFirst();
-        if(episodioBuscado.isPresent()){
-            System.out.println("Episodio encontrado");
-            System.out.println("Los datos son: "+ episodioBuscado.get());
-        } else {
-            System.out.println("Episodio no encontrado");
-        }
+//        System.out.println("Por favor escriba el titulo del capitulo que desea ver: ");
+//        var pedazoTitulo = teclado.nextLine();
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo.toUpperCase()))
+//                .findFirst();
+//        if(episodioBuscado.isPresent()){
+//            System.out.println("Episodio encontrado");
+//            System.out.println("Los datos son: "+ episodioBuscado.get());
+//        } else {
+//            System.out.println("Episodio no encontrado");
+//        }
+
+        Map<Integer,Double> evaluacionesPorTemporada = episodios.stream()
+                .filter(e-> e.getEvaluacion() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getEvaluacion)));
+
+        System.out.println(evaluacionesPorTemporada);
+
+        // Manejando estadísticas
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e-> e.getEvaluacion() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getEvaluacion));
+        System.out.println("Media de las evaluaciones: "+est.getAverage());
+        System.out.println("Episodio mejor evaluado: "+ est.getMax());
+        System.out.println("Episodio peor evaluado: "+ est.getMin());
     }
 }
